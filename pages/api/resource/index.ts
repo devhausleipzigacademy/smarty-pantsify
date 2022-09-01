01a877edfc27ec6286416781e4036a5fe60d7ebf
Franz Wollang <franzwollang@Franzs-MacBook-Pro.local>
2022-09-01 16:52:49 +0200

    Add resource model

---
+++ pages/api/resource/index.ts
@@ -0,0 +1,27 @@
+import { Resource } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../prisma/db";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<{ id: string } | { message: string; error: any }>
+) {
+    try {
+        if (req.method === "POST") {
+            const data = req.body;
+
+            const resource = await prisma.resource.create({
+                data: {
+                    ...data,
+                },
+            });
+
+            res.status(201).json({ id: resource.id });
+        }
+    } catch (err) {
+        res.status(404).send({
+            message: "Oops, looks like we made a mistake.",
+            error: err,
+        });
+    }
+}
