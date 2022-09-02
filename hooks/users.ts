4823a2e5acbad62362d15500d0579eafc43889cd
stephanr <ritscher@gmx.com>
2022-09-02 14:02:49 +0200

    created user api hooks

---
+++ hooks/users.ts
@@ -0,0 +1,54 @@
+import { User } from "@prisma/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export function useUser(userId: string) {
+	return useQuery(
+		["users", userId],
+		() =>
+			fetch(`http://localhost:3000/api/user/${userId}`).then((res) =>
+				res.json()
+			),
+		{ enabled: Boolean(userId) }
+	);
+}
+
+type UserPrePrisma = {
+	name: String;
+	image: String;
+};
+
+export function useUsers() {
+	return useQuery<User[]>(["users"], () =>
+		fetch("http://localhost:3000/api/user").then((res) => res.json())
+	);
+}
+
+export function useCreateUser() {
+	const queryClient = useQueryClient();
+	return useMutation(
+		["users", "create"],
+		(user: UserPrePrisma) => {
+			return fetch("http://localhost:3000/api/user", {
+				method: "POST",
+				body: JSON.stringify(user),
+			}).then((res) => res.json());
+		},
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries(["users"]);
+			},
+		}
+	);
+}
+
+export function useDeleteUser(userId: string) {
+	const queryClient = useQueryClient();
+	return useMutation(
+		["users", userId, "delete"],
+		() =>
+			fetch(`http://localhost:3000/api/user/${userId}`, {
+				method: "DELETE",
+			}),
+		{ onSuccess: () => queryClient.invalidateQueries(["users"]) }
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,13 @@
-import { User } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
 import { RecommendedHome } from "../components/home/RecommendedHome";
 import { StatusHome } from "../components/home/StatusHome";
-import { TaskListHome } from "../components/home/TaskListHome";
+// import { TaskListHome } from "../components/home/TaskListHome";
+import { useUser } from "../hooks/users";
 
 const Home: NextPage = () => {
 	//TODO: Create auth so that we don't have to hard code here
-	const userId = 1234;
-	const {
-		data: user,
-		isLoading: userLoading,
-		error,
-	} = useQuery<User>(
-		["user", userId],
-		() =>
-			fetch(`http://localhost:3000/api/user/${userId}`).then((res) =>
-				res.json()
-			),
-		{ enabled: Boolean(userId) }
-	);
+	const userId = "133b6003-1cc4-4394-aa95-c795f710b516";
+	const { data: user, isLoading: userLoading, error } = useUser(userId);
 	if (userLoading) return <p>Loading</p>;
 	if (!userLoading && user === undefined) return <p>user not found</p>;
 	return (
@@ -27,12 +15,12 @@ const Home: NextPage = () => {
 			<h2 className="text-2xl font-light ">Welcome back, {user.name}!</h2>
 
 			<div id="upperElements" className="flex flex-row w-full gap-4 h-80 ">
-				<div id="upcomingTasks" className="w-2/3 h-full card-style">
+				{/* <div id="upcomingTasks" className="w-2/3 h-full card-style">
 					<TaskListHome />
 				</div>
 				<div id="statusBoard" className="w-1/3 h-full card-style">
 					<StatusHome />
-				</div>
+				</div> */}
 			</div>
 			<div id="recommendedBlogposts" className="w-full card-style h-96">
 				<RecommendedHome />
