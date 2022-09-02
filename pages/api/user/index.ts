6aafbb1c3e8eac3297cd669ab9a3a0a57be58485
Franz Wollang <franzwollang@Franzs-MacBook-Pro.local>
2022-09-02 10:40:39 +0200

    Added some validation to user endpoints & fixed response codes

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -3,13 +3,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { User } from "@prisma/client";
 import { prisma } from "../../../prisma/db";
 
+import { z, ZodError } from "zod";
+
+export const postUser = z.object({
+    name: z.string(),
+    image: z.string(),
+});
+
+export type PostUser = z.infer<typeof postUser>;
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<{ id: string } | { message: string; error: any }>
 ) {
     try {
         if (req.method === "POST") {
-            const data = req.body;
+            const data = postUser.parse(req.body);
 
             const user = await prisma.user.create({
                 data: {
@@ -20,8 +29,15 @@ export default async function handler(
             res.status(201).json({ id: user.id });
         }
     } catch (err) {
-        res.status(404).send({
-            message: "Oops, looks like we made a mistake.",
+        if (err instanceof ZodError) {
+            res.status(422).send({
+                message: "Invalid user.",
+                error: err,
+            });
+        }
+
+        res.status(400).send({
+            message: "The server encountered an error.",
             error: err,
         });
     }
